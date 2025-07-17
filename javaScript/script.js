0d6601d0f7dca86d3845e7dd5c362146acc015e3
Roberto <robertosarta.rm@gmail.com>
2025-07-18 00:32:26 +0200

    Añado el menu desplegable para movil, falta hacerlo funcional en pc

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,7 +1,8 @@
-const menuToggle = document.getElementById("menuToggle");
-const navMenu = document.getElementById("navMenu");
+const menuToggle = document.getElementById("menuButton");
+const navMenu = document.getElementById("headerMenu");
 
 menuToggle.addEventListener("click", () => {
+     console.log("Botón pulsado");
     navMenu.classList.toggle("open");
     menuToggle.classList.toggle("open"); // esto activa la animación
 });
