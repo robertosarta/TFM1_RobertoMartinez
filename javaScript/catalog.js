dd033fa3b1eaf11f4cbb0edda754c6d71324bc71
Roberto <robertosarta.rm@gmail.com>
2025-07-19 00:01:12 +0200

    añado las imagenes de los visores en la pantalla principal pero no consigo hacerlo de forma procedural. De momento quedan puestas cada una con su script

---
+++ javaScript/catalog.js
@@ -0,0 +1,55 @@
+let products = {
+        Tipche: {
+            picture: "/images/catalog/Tipche.JPG",
+            name: "TiPche",
+            price: "50€",
+            about:
+                "Guerrero de fantasia con un toque aborigen",
+            cart: 0,
+            uid: "6e47b68d13a0413d8fd5fa248a639e8b",
+        },
+        Morpheus: {
+            picture: "https://previews.123rf.com/images/bassanini/bassanini1801/bassanini180100023/94158630-chromed-mechanical-key-used-on-a-white-background.jpg",
+            name: "Morpheus",
+            price: "33€",
+            about:
+                "Demonio guerrero",
+            cart: 0,
+            uid: "aea0f05978384dc992398ab122c7f2f8",
+        },
+        StitchedSwordsman: {
+            picture: "Stitched Swordsman",
+            name: "/images/catalog/StitchedSwordsman.jpg",
+            price: "63€",
+            about:
+                "This project was made for Stylised Creation course at Digital Arts and Entertainment. Ill eventually also make an Art Station post. This was an extremely fun and quick character to make! I tried to explore some new ground with mixing pbr and graphic inked shading (ie Darkest Dungeon, but with pbr aspects)! Huge thanks to Thiago Lehmann for allowing me to make his concept in 3d.",
+            cart: 0,
+            uid: "ab4cadb444a44f7883995ddb3bf3d087",
+        },
+        Witch: {
+            picture: "/images/catalog/Witch.JPG",
+            name: "Witch",
+            price: "75€",
+            about:
+                "Final assignment for character creation. Based on the work of Pang.",
+            cart: 0,
+            uid: "a4f51228a33146159b2c65385baa2f51",
+        },
+    };
+
+// let cart = [];
+// const savedCart = JSON.parse(localStorage.getItem('carrito'));
+// if (savedCart){
+//     cart = savedCart   //actualiza el array desde el otro js
+// }else {
+//     let arrayItems = Object.values(products); //lo vuelve a crear si no existe (convierte products en array)
+//     cart = arrayItems.map(p => ({
+//     picture: p.picture,
+//     name: p.name,
+//     price: p.price,
+//     about: p.about,
+//     cart: p.cart,
+//     uid: p.uid,
+// }));
+// }
+
--- javaScript/scriptVisor.js
+++ javaScript/scriptVisor.js
@@ -1,22 +1,88 @@
-const iframe = document.getElementById('api-frame');
-const uid = '6e47b68d13a0413d8fd5fa248a639e8b';
+// let cart = [];
+// const savedCart = JSON.parse(localStorage.getItem('carrito'));
+// if (savedCart) {
+//     cart = savedCart   //actualiza el array desde el otro js
+// } else {
+//     let arrayItems = Object.values(products); //lo vuelve a crear si no existe (convierte products en array)
+//     cart = arrayItems.map(p => ({
+//         picture: p.picture,
+//         name: p.name,
+//         price: p.price,
+//         about: p.about,
+//         cart: p.cart,
+//         uid: p.uid,
+//     }));
+// }
 
+// const productosPorNombre = {};
+// cart.forEach(p => {
+//     productosPorNombre[p.name.toLowerCase()] = p;
+// });
+
+// let presentationModels = [
+//     "witch",
+//     "morpheus",
+//     "tipche"
+// ];
+
+// function searchUid(key) {
+//     const product = productosPorNombre[key.toLowerCase()];
+//     if (!product) return null;
+//     return product.uid;
+// }
+
+let uid1 = "6e47b68d13a0413d8fd5fa248a639e8b";
+let iframe1 = document.getElementById(`api-frame-TiPche`);
 // By default, the latest version of the viewer API will be used.
-const client = new Sketchfab(iframe);
-
-// Alternatively, you can request a specific version.
-// var client = new Sketchfab( '1.12.1', iframe );
-
-client.init(uid, {
+let client1 = new Sketchfab(iframe1);
+client1.init(uid1, {
     success: function onSuccess(api) {
         api.start();
-        api.addEventListener('viewerready', function () {
+        api.addEventListener("viewerready", function () {
             // API is ready to use
             // Insert your code here
-            console.log('Viewer is ready');
+            console.log("Viewer is ready");
         });
     },
     error: function onError() {
-        console.log('Viewer error');
-    }
+        console.log("Viewer error");
+    },
 });
+
+const uid2 = "a4f51228a33146159b2c65385baa2f51";
+const iframe2 = document.getElementById(`api-frame-Witch`);
+// By default, the latest version of the viewer API will be used.
+const client2 = new Sketchfab(iframe2);
+client2.init(uid2, {
+    success: function onSuccess(api) {
+        api.start();
+        api.addEventListener("viewerready", function () {
+            // API is ready to use
+            // Insert your code here
+            console.log("Viewer is ready");
+        });
+    },
+    error: function onError() {
+        console.log("Viewer error");
+    },
+});
+
+
+const uid3 = "aea0f05978384dc992398ab122c7f2f8";
+const iframe3 = document.getElementById(`api-frame-Morpheus`);
+// By default, the latest version of the viewer API will be used.
+const client3 = new Sketchfab(iframe3);
+client3.init(uid3, {
+    success: function onSuccess(api) {
+        api.start();
+        api.addEventListener("viewerready", function () {
+            // API is ready to use
+            // Insert your code here
+            console.log("Viewer is ready");
+        });
+    },
+    error: function onError() {
+        console.log("Viewer error");
+    },
+});
+
