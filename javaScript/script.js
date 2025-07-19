ecb4ed440d745ddea3dcf53522657d945531e11f
Roberto <robertosarta.rm@gmail.com>
2025-07-19 20:38:50 +0200

    Añado a las cards de presentacion un fondo bajo con difuminacion copiando warhammer.com y les pongo un efecto de parallax para que sean mas vistosas. Queda hacer 2 mas y pasar a la siguiente seccion

--- javaScript/script.js
+++ javaScript/script.js
@@ -5,3 +5,21 @@ menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("open");
     menuToggle.classList.toggle("open"); // esto activa la animación
 });
+
+// PARALAX CARDS
+document.querySelectorAll('.card-custom').forEach(card => {
+    const frontImg = card.querySelector('.card-img_front');
+
+    card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = (e.clientX - rect.left) / rect.width -0.5;
+        const y = (e.clientY - rect.top) / rect.height -0.5;
+
+        frontImg.style.transform = `translate(${x * 1.5}rem, ${y * 1.5}rem)`; 
+    })
+
+    card.addEventListener('mouseleave', () => {
+        frontImg.style.transform = 'translate(0, 0';
+    })
+});
+
