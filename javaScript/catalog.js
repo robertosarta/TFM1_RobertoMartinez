3de79739373fdd376ba91a5c1b5225715be68fdc
Roberto <robertosarta.rm@gmail.com>
2025-07-25 21:42:07 +0200

    Logica del catalogo funcionando, la API devuelve los datos y se cogen los necesarios para pintar las cards y guardar valores que interesan, la estrella actualiza la interaccion y al clicar en las cards guarda la info de la misma en el local para poder usarla mas adelante y asi solo guardamos con las que se interacciona

--- javaScript/catalog.js
+++ javaScript/catalog.js
@@ -1,9 +1,168 @@
-fetch(`https://api.sketchfab.com/v3/search?type=models&${filter}`, {
-    headers: {
-        'Authorization': 'Token 6c0d4578e96b4b10b04fba257249d6a1'
+let cart = [];
+let savedCart = JSON.parse(localStorage.getItem('cart')); /*cuidado para debuggear esto no deja reiniciar los stats de las cards, starMarked no actualiza aunque hagas localStorage.clear()*/
+
+if (savedCart){
+    cart = savedCart   //actualiza el array desde el otro js
+}
+
+function randomPrice() {
+    return Math.floor(Math.random() * 200) + 1;
+}
+
+function chooseImage(array) {
+    if (!array || array.length === 0) {
+        return { url: "./images/fallback.jpg" };
+    } else {
+        let choosenHeight = array[0];
+        array.forEach(image => {
+            if (image.height > choosenHeight.height) {
+                choosenHeight = image
+            }
+        });
+        return choosenHeight;
     }
-})
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.results);
+}
+
+function saveItemData(item) {
+    if (cart.find(i => i.uid === item.uid)) {
+        return;
+    }else {
+        const savedItem = {
+            picture: chooseImage(item.thumbnails.images).url,
+            name: item.name,
+            price: randomPrice(),
+            description: item.description,
+            cart: item.cart || false,
+            uid: item.uid,
+            commentCount: item.commentCount,
+            faceCount: item.faceCount,
+            likeCount: item.likeCount,
+            tags: item.tags,
+            starMarked: false
+        };
+    cart.push(savedItem);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    console.log("Item guardado:", savedItem);
+    }
+} 
+
+function crearElemento(tipo, classElement, text, src, alt, aria) {
+    const elem = document.createElement(tipo);
+    if (classElement) elem.className = (classElement);
+    if (text) elem.textContent = (text);
+    if (src) elem.src = (src);
+    if (alt) elem.alt = (alt);
+    if (aria) elem.setAttribute("aria-label", aria);
+    return elem;
+};
+
+const modelSection = document.querySelector(".model-section");
+function createCard(models) {
+    models.forEach(model => {
+        const savedModel = cart.find(item => item.uid === model.uid);
+        if (savedModel) {
+            model.likeCount = savedModel.likeCount;
+            model.starMarked = savedModel.starMarked;
+        }else {
+            model.starMarked = false;
+        }
+
+        const imageUrl = chooseImage(model.thumbnails.images).url;
+        const modelCard = crearElemento("div", "model-card");
+        const imageContainer = crearElemento("div", "image-container");
+        const modelImage = crearElemento("img", "model-card-iframe", null, `${imageUrl}`);
+
+        const modelInfoContainer = crearElemento("div", "model-info-container");
+        const modelName = crearElemento("h4", "model-info_name fs-text font-text", `${model.name}`);
+        const modelInfoReactionsContainer = crearElemento("div", "model-info_reactions-container");
+
+        const modelInfoReactionsContainerViews = crearElemento("div", "model-info_reactions-container_element");
+        const iconViews = crearElemento("i", "bi bi-eye-fill fs-text font-text");
+        const viewCount = crearElemento("h5", "fs-text font-text", `${model.viewCount}`);
+
+        const modelInfoReactionsContainerComments = crearElemento("div", "model-info_reactions-container_element");
+        const iconComments = crearElemento("i", "bi bi-chat-fill fs-text font-text");
+        const commentCount = crearElemento("h5", "fs-text font-text", `${model.commentCount}`);
+
+        const modelInfoReactionsContainerLikes = crearElemento("div", "model-info_reactions-container_element");
+        const iconLikes = crearElemento("i", "bi bi-star-fill fs-text font-text");
+        const likesCount = crearElemento("h5", "fs-text font-text", `${model.likeCount}`);
+
+
+        modelSection.appendChild(modelCard);
+        modelCard.appendChild(imageContainer);
+        imageContainer.appendChild(modelImage);
+
+        modelCard.appendChild(modelInfoContainer);
+        modelInfoContainer.appendChild(modelName);
+        modelInfoContainer.appendChild(modelInfoReactionsContainer);
+        modelInfoReactionsContainer.append(modelInfoReactionsContainerViews, modelInfoReactionsContainerComments, modelInfoReactionsContainerLikes);
+        modelInfoReactionsContainerViews.append(iconViews, viewCount);
+        modelInfoReactionsContainerComments.append(iconComments, commentCount);
+        modelInfoReactionsContainerLikes.append(iconLikes, likesCount);
+
+        modelName.addEventListener("click", () =>{
+            saveItemData(model);
+        });
+
+        modelInfoReactionsContainerComments.addEventListener("click", () =>{
+            saveItemData(model);
+            /*y hacer el link a la pagina de visualizacion*/
+        });
+
+        modelInfoReactionsContainerLikes.addEventListener("click", () =>{
+            if (model.starMarked) {
+                return;
+            }
+            
+            model.likeCount++
+            model.starMarked = true;
+
+            const itemInCart = cart.find(i => i.uid === model.uid);
+            if (!itemInCart) {
+                cart.push({...model});
+            }else {
+                itemInCart.likeCount = model.likeCount;
+                model.starMarked = true;
+            }
+            likesCount.textContent = model.likeCount
+            
+            localStorage.setItem('cart', JSON.stringify(cart));
+            console.log("Cart actualizado:", cart);
+            console.log("HOLA")
+        });
     });
+}
+
+
+
+let nextPageURL = null;
+function fetchFilter(filter, createCard, url = null) {
+    const apiUrl = url || `https://api.sketchfab.com/v3/search?type=models&q=${filter}&count=24`;
+    fetch(apiUrl, {
+        headers: {
+            'Authorization': 'Token 6c0d4578e96b4b10b04fba257249d6a1'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            const models = data.results;
+            createCard(models);
+            nextPageURL = data.next;
+            console.log("Respuesta completa de la API:", data); /*PARA VER QUE ME DEVUELVE*/
+        });
+
+}
+
+const loadNext = document.querySelector(".model-section_load-next");
+loadNext.addEventListener("click", () => {
+    if (nextPageURL) {
+        fetchFilter(null, createCard, nextPageURL)
+    } else {
+        window.alert("No hay mas resultados");
+    }
+});
+
+
+
+fetchFilter("demon", createCard);
--- javaScript/database.js
+++ javaScript/database.js
@@ -1,64 +1,35 @@
-let products = {
-        Tipche: {
-            picture: "/images/catalog/Tipche.JPG",
-            name: "TiPche",
-            price: "50€",
-            about:
-                "Guerrero de fantasia con un toque aborigen",
-            cart: 0,
-            uid: "6e47b68d13a0413d8fd5fa248a639e8b",
-        },
-        Morpheus: {
-            picture: "https://previews.123rf.com/images/bassanini/bassanini1801/bassanini180100023/94158630-chromed-mechanical-key-used-on-a-white-background.jpg",
-            name: "Morpheus",
-            price: "33€",
-            about:
-                "Demonio guerrero",
-            cart: 0,
-            uid: "aea0f05978384dc992398ab122c7f2f8",
-        },
-        StitchedSwordsman: {
-            picture: "/images/catalog/StitchedSwordsman.jpg",
-            name: "StitchedSwordsman",
-            price: "63€",
-            about:
-                "This project was made for Stylised Creation course at Digital Arts and Entertainment. Ill eventually also make an Art Station post. This was an extremely fun and quick character to make! I tried to explore some new ground with mixing pbr and graphic inked shading (ie Darkest Dungeon, but with pbr aspects)! Huge thanks to Thiago Lehmann for allowing me to make his concept in 3d.",
-            cart: 0,
-            uid: "ab4cadb444a44f7883995ddb3bf3d087",
-        },
-        Witch: {
-            picture: "/images/catalog/Witch.JPG",
-            name: "Witch",
-            price: "75€",
-            about:
-                "Final assignment for character creation. Based on the work of Pang.",
-            cart: 0,
-            uid: "a4f51228a33146159b2c65385baa2f51",
-        },
-        Temple: {
-            picture: "/images/catalog/Witch.JPG",
-            name: "Temple",
-            price: "75€",
-            about:
-                "Explore the mystique of forgotten civilizations with this atmospheric 3D scene featuring an ancient temple entrance. The heavy stone doors are adorned with mysterious carvings, surrounded by moss-covered walls and subtle lighting that evokes a sense of sacred secrecy.",
-            cart: 0,
-            uid: "f3e5a8e7f36a4c7496f5c775acf010b7",
-        },
-    };
+// let products = {
+//         Example:{
+//             picture:"",
+//             name: "",
+//             price: "", /*añadirlo de forma aleatoria entre 1 y 200*/
+//             description: "",
+//             cart: 0,
+//             uid: "",
+//             commentCount: "",
+//             faceCount: "",
+//             likeCount: "",
+//             tags: "",
+//         },
+//     };
 
-let cart = [];
-const savedCart = JSON.parse(localStorage.getItem('carrito'));
-if (savedCart){
-    cart = savedCart   //actualiza el array desde el otro js
-}else {
-    let arrayItems = Object.values(products); //lo vuelve a crear si no existe (convierte products en array)
-    cart = arrayItems.map(p => ({
-    picture: p.picture,
-    name: p.name,
-    price: p.price,
-    about: p.about,
-    cart: p.cart,
-    uid: p.uid,
-}));
-}
+// let cart = [];  //creo un carrito vacio
+// const savedCart = JSON.parse(localStorage.getItem('carrito'));
+// if (savedCart){
+//     cart = savedCart   //actualiza el array desde el otro js
+// }else {
+//     let arrayItems = Object.values(products); //lo vuelve a crear si no existe (convierte products en array)
+//     cart = arrayItems.map(p => ({
+//     picture: p.picture,
+//     name: p.name,
+//     price: p.price,
+//     description: p.description,
+//     cart: p.cart,
+//     uid: p.uid,
+//     commentCount: p.commentCount,
+//     faceCount: p.faceCount,
+//     likeCount: p.likeCount,
+//     tags: p.tags
+// }));
+// }
 
